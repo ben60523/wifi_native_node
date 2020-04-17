221424a32b9432c76a6b01b43ce3c81108ff9611
ssyang <ben60523@gmail.com>
2020-04-17 15:48:40 +0800

    Set wlanrawdata to NULL in WlanScan

--- wifi_scan.cc
+++ wifi_scan.cc
@@ -54,9 +54,9 @@ napi_value Scan(napi_env env, napi_callback_info info)
     for (ifaceNum = 0; ifaceNum < (int)pIfList->dwNumberOfItems; ifaceNum++)
     {
       pIfInfo = (WLAN_INTERFACE_INFO *)&pIfList->InterfaceInfo[ifaceNum];
-      WLAN_RAW_DATA WlanRawData;
+      PWLAN_RAW_DATA WlanRawData = NULL;
       wprintf(L"%ls Scanning...\n", pIfInfo->strInterfaceDescription);
-      dwResult = WlanScan(hClient, &pIfInfo->InterfaceGuid, pDotSSid, &WlanRawData, NULL);
+      dwResult = WlanScan(hClient, &pIfInfo->InterfaceGuid, pDotSSid, WlanRawData, NULL);
       if (dwResult != ERROR_SUCCESS)
       {
         wprintf(L"WlanScan failed with error: %u\n", dwResult);
